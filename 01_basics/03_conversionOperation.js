b40b84270e5afeca7b28c4379cecfa04c3caacf5
SureshBugaliya877 <sureshbugaliya13@gmail.com>
2024-02-10 21:46:37 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+let score = 33;
+let scores = "33";
+let scoren = "33ab";
+let testnull = null; // null= typeof object value 0
+                    //undifend = typeof undifend value nan
+                    //ture = typeof boolean value 1
+                    //hitesh = typeof string value nan
+
+// console.log(typeof score);
+// console.log(typeof(scores));
+// console.log(typeof(scoren));
+// console.log(typeof(testnull));
+
+// let valueInNumber = Number(scores);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let valueInNumbern = Number(scoren);
+// console.log(typeof valueInNumbern);
+// console.log(valueInNumbern);
+
+// let valueInnull = Number(testnull);
+// console.log(typeof valueInnull);
+// console.log(valueInnull);
+
+
+
+
+// "33" => 33
+// "33ab" => nan
+//true => 1; false =>0
+
+// let isloggedin = 1;
+// let booleanisloggedin = Boolean(isloggedin);
+// console.log(booleanisloggedin);
+
+// yadi 1 h to true aayga , mt (khali) h to false aayga or string like "suresh" h to true aayga
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let somenumber = 33
+let stringnumber = String(somenumber);
+console.log(typeof stringnumber);
+console.log(stringnumber);
