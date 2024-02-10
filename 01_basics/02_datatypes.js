ecef77015cf39afcf482f6b67937811a1936c1aa
SureshBugaliya877 <sureshbugaliya13@gmail.com>
2024-02-10 20:31:31 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3);  // we are using node js note use browser
+
+console.log(3+3);
+
+console.log("suresh");
+
+let name = "suresh"
+let age = 18
+let isloggrdin = false
+let state; //undefiend 
+let temp = null
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefiend => 
+// symbol => unique ness
+
+
+//object
+
+console.log(typeof "suresh");
+console.log(typeof age);
+console.log(typeof null);            //*************** type = object ********************* */
+console.log(typeof undefined);
